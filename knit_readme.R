25baba5e2a27ee9fcdcca5718c356aa65844a2fd
jevansbio <32939196+jevansbio@users.noreply.github.com>
2021-12-02 14:16:19 +0100

    Delete knit_readme.R

--- knit_readme.R
+++
@@ -1,5 +0,0 @@
-rmarkdown::render('C:\\Users\\Julian\\Documents\\GitHub\\net2blend\\net2blendR\\vignettes\\net2blend_help.Rmd',
-                  output_format=rmarkdown::github_document(toc =TRUE, toc_depth = 2, html_preview= FALSE),
-                  output_file = 'C:\\Users\\Julian\\Documents\\GitHub\\net2blend\\README.md',
-                  knit_root_dir="C:\\Users\\Julian\\Documents\\GitHub\\net2blend\\net2blendR\\vignettes\\")
-
